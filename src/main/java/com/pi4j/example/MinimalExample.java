d2c81646ac76452d6f391b9410f32712e11cf5f2
Frank Delporte <frank@webtechie.be>
2024-04-24 14:08:07 +0200

    Added board info output

--- src/main/java/com/pi4j/example/MinimalExample.java
+++ src/main/java/com/pi4j/example/MinimalExample.java
@@ -28,6 +28,7 @@ package com.pi4j.example;
  */
 
 import com.pi4j.Pi4J;
+import com.pi4j.boardinfo.util.BoardInfoHelper;
 import com.pi4j.io.gpio.digital.DigitalInput;
 import com.pi4j.io.gpio.digital.DigitalState;
 import com.pi4j.io.gpio.digital.PullResistance;
@@ -100,6 +101,24 @@ public class MinimalExample {
         PrintInfo.printLoadedPlatforms(console, pi4j);
         PrintInfo.printDefaultPlatform(console, pi4j);
         PrintInfo.printProviders(console, pi4j);
+
+        // ------------------------------------------------------------
+        // Output Pi4J Board information
+        // ------------------------------------------------------------
+        // When the Pi4J Context is initialized, a board detection is 
+        // performed. You can use this info in case you need board-specific
+        // functionality.
+        // OPTIONAL
+        console.println("Board model: " + pi4j.boardInfo().getBoardModel().getLabel());
+        console.println("Operating system: " + pi4j.boardInfo().getOperatingSystem());
+        console.println("Java versions: " + pi4j.boardInfo().getJavaInfo());
+        // This info is also available directly from the BoardInfoHelper, 
+        // and with some additional realtime data.
+        console.println("Board model: " + BoardInfoHelper.current().getBoardModel().getLabel());
+        console.println("Raspberry Pi model with RP1 chip (Raspberry Pi 5): " + BoardInfoHelper.usesRP1());
+        console.println("OS is 64-bit: " + BoardInfoHelper.is64bit());
+        console.println("JVM memory used (MB): " + BoardInfoHelper.getJvmMemory().getUsedInMb());
+        console.println("Board temperature (°C): " + BoardInfoHelper.getBoardReading().getTemperatureInCelsius());
 
         // Here we will create the I/O interface for a LED with minimal code.
         var led = pi4j.digitalOutput().create(PIN_LED);
